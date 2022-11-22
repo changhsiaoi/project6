de1e6499969a44788e0ab1f6c924b2483ce75355
changhsiaoi <vikki0523@gmail.com>
2022-11-22 22:47:27 +0800

    modify

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import private
-print(private.secret.open_weather_key)
+import requests
+
+
+def main():
+    api_key =private.secret.open_weather_key
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q=Hualien,tw&APPID={api_key}&lang=zh_tw&units=metric"
+    response = requests.get(url)
+    if response.ok:
+        print("下載成功")
+        allDate = (response.json())
+        city = allDate['city']
+        print(city)
+
+if __name__ == "__main__":
+    main()
